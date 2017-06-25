0488579fc863bb7b97cd2b804abeda6fbbdedab4
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-06-25 18:10:05 -0400

    add annotations on long press added and set up navigation bar

--- VirtualTourist/MapsViewController.swift
+++ VirtualTourist/MapsViewController.swift
@@ -9,15 +9,41 @@
 import UIKit
 import MapKit
 
-class MapsViewController: UIViewController {
+class MapsViewController: UIViewController, MKMapViewDelegate {
 
+    var deleteMode = false
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // TODO: Add action
+        setUpUI()
+        addLongPress()
+
+        
     }
 
+    func setUpUI() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: nil)
+    }
     
-
+    func addLongPress() {
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(action))
+        longPress.minimumPressDuration = 1.0
+        map.addGestureRecognizer(longPress)
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer){
+        if gestureRecognizer.state == .ended {
+            let touchPoint = gestureRecognizer.location(in: map)
+            let newCoordinates = map.convert(touchPoint, toCoordinateFrom: map)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            map.addAnnotation(annotation)
+        }
+    }
 
 }
 
